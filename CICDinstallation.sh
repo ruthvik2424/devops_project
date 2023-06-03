7e2f867c9a9ec0e1524417e67c6db3586bede554
Ruthvik <ruthvikshinde553@gmail.com>
2023-06-03 13:54:22 +0530

    changes in CICD file

--- CICDinstallation.sh
+++ CICDinstallation.sh
@@ -10,28 +10,6 @@ sleep 1
 echo "Need of Container Technology....."
 sleep 1
 sudo yum install -y podman
-echo "Starting the Installion of Jenkins......"
-sleep 1
-podman pull docker.io/devopsjourney1/jenkins-blueocean:2.332.3-1 && podman tag devopsjourney1/jenkins-blueocean:2.332.3-1 myjenkins-blueocean:2.332.3-1
-echo "Creating a Network For jenkins....."
-sleep 1
-podman network create jenkins
-echo "Starting the Jenkins Container......"
-sleep 1
-podman run --name jenkins-blueocean --restart=on-failure --detach \
-  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
-  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
-  --publish 8080:8080 --publish 50000:50000 \
-  --volume jenkins-data:/var/jenkins_home \
-  --volume jenkins-docker-certs:/certs/client:ro \
-  myjenkins-blueocean:2.332.3-1
-echo "Extracting the Password...... (copy karlo login kar paoge)"
-sleep 1
-docker exec jenkins-blueocean cat /var/jenkins_home/secrets/initialAdminPassword
-echo "Its running on https://localhost:8080/"
-curl https://localhost:8080/
-echo "Finally the Jenkins installation is Completed"
-sleep 1
 echo "Now lets install minikube for readytodeploy cluster for (x86_64 if your architecture is different refer offical documentation)"
 sleep 1
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
