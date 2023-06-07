8edd28c19374b7b34023be07b6991c86e35be534
Ruthvik Shinde <121545155+ruthvik2424@users.noreply.github.com>
2023-06-07 15:15:35 +0530

    Update CICDinstallation.sh

--- CICDinstallation.sh
+++ CICDinstallation.sh
@@ -39,14 +39,13 @@ kubectl create namespace argocd
 echo "Installing argocd on minikube cluster........."
 sleep 1
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-echo "Waiting for 20 sec for argocd installation"
-sleep 20
-echo "Extracting the Initial admin-password for login (copy karlo)"
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
 sleep 1
-argocd admin initial-password -n argocd
-echo "Port-forwarding the argocd service for acccessing UI"
-sleep 1
-kubectl port-forward svc/argocd-server -n argocd 8080:443
+echo "run this commands after installation"
+#argocd admin initial-password -n argocd
+#kubectl port-forward svc/argocd-server -n argocd 8080:443
 echo "Here the whole installation of all different tools finished get ready to configure this tools :-) :-)"
 sleep 3
 echo "Finishing ......."
